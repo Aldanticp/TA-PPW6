37fc00c7f47a4dd064a0571131cbbd8f35902cfc
Aldanticp <aldanticp@gmail.com>
2025-12-01 19:01:02 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const API_CONFIG = {
+    API_KEY: '7dce5a029e7249ac990d2f064f8e5e2a', 
+    BASE_URL: 'https://api.openweathermap.org/data/2.5',
+    DEFAULT_CITY: 'Bandar Lampung',
+    AUTO_UPDATE_DELAY: 300000,
+    FAV_STORAGE_KEY: 'weatherDashboardFavorites',
+    SETTINGS_STORAGE_KEY: 'weatherDashboardSettings'
+};
